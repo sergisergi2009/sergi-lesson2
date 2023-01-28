78dcc0b80fa199fb52d811364f9e1856836e0ccf
sergisergi2009 <sergi.pharsenadze@gmail.com>
2023-01-28 13:41:09 +0400

    day2 code commit

---
+++ lesson1.py
@@ -0,0 +1,13 @@
+name = "sergi"
+surname = "pharsenadze"
+age = 13
+
+print("me mqvia " + name + " chemi gvaria " + 
+    surname + " me var " + str(age) + " wlis")
+
+
+year = 5
+age += year
+
+
+print("gavida 5 weli da var " + str(age) + " wlis")
---
+++ lesson2.py
@@ -0,0 +1,1 @@
+print("this is lesson 2")
